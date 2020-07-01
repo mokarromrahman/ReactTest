817b8e58ce90a54beabc7df1333e1117ef62a691
Mokarrom Rahman <mrahman0000@gmail.com>
2020-07-01 17:23:34 -0600

    Worked on more of the linkedin learning react course.

--- LinkedIn_Learning/ch01/01_02/start/hello_start/src/index.js
+++ LinkedIn_Learning/ch01/01_02/start/hello_start/src/index.js
@@ -1,17 +1,113 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+// let city = {
+//   "name": "Madrid",
+//   "country": "Spain"
+// };
+
+// function Hello({library, message, number})
+// {
+//   //console.log(Object.keys(props));
+//   return (
+//   <div>
+//     <h1>Welcome to {library}</h1>
+//     <p>{message}</p>
+//     <ul>
+//       <li>{number} props total</li>
+//     </ul>
+//   </div>);
+// }
+// ReactDOM.render(
+//     <Hello library = "vue.js" message="have fun!" number={3}/>,
+//   document.getElementById('root')
+// );
+
+// function Lake({name})
+// {
+//   return(
+//     <h1>{name}</h1>
+//   );
+// }
+
+// function App()
+// {
+//   return(
+//     <div>
+//       <Lake name="Lake Tahoe"></Lake>
+//       <Lake name="Angora Lake"></Lake>
+//       <Lake name="Shirley Lake"></Lake>
+//     </div>
+//   );
+// }
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById("root")
+// );
+
+// const lakeList = ["Echo Lake", "Maud Lake", "Casecade Lake"];
+
+// //lakes has to be inside {} so that it is treated as an array and not an object
+// function App({lakes})
+// {
+//   return (
+//   <ul>
+//       {lakes.map(lake => (
+//       <li>
+//         {lake}
+//       </li>))}
+//   </ul>)
+// }
+
+// ReactDOM.render(
+//   <App lakes={lakeList} />,
+//   document.getElementById("root")
+// );
+
+// const lakeList = [
+  
+//     {id:"1", name:"Echo", trailhead:"Echo"},
+//     {id:"2", name:"Maud", trailhead:"Wrights"},
+//     {id:"3", name:"Velma", trailhead:"Bayview"}
+// ];
+
+// //lakes has to be inside {} so that it is treated as an array and not an object
+// function App({lakes})
+// {
+//   return (
+//     <div>
+//       {lakes.map(lake => (
+//         <div>
+//           <h2>{lake.name}</h2>
+//           <p>Accessed by: {lake.trailhead}</p>
+//         </div>
+//       ))}
+//     </div>
+//   )
+// }
+
+// ReactDOM.render(
+//   <App lakes={lakeList} />,
+//   document.getElementById("root")
+// );
+
+const list = [1,2,3,4,5];
+
+//lakes has to be inside {} so that it is treated as an array and not an object
+function App({items})
+{
+return (
+  <ul>
+    {items.map(item => (
+      <li key={item.toString()}>{item}</li>
+    ))}
+  </ul>
+)
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+<App items={list} />,
+document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
